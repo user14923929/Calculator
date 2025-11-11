0c626e4ccaa9df6c9bce5226994ad956e82736e6
IlliaCoder <i68281124@gmail.com>
2025-11-11 11:22:04 +0200

    refactor: simplify calculation logic by allowing input of math expressions and removing unused fields

--- assets/js/calculation.js
+++ assets/js/calculation.js
@@ -3,61 +3,19 @@ const answer = document.querySelector('input[name="answer"]');
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    const num1 = parseInt(form.querySelector('input[name="num1"]').value);
-    const num2 = parseInt(form.querySelector('input[name="num2"]').value);
-    const operator = form.querySelector('select[name="operator"]').value;
+    const inputValue = form.querySelector('input[name="input"]').value;
     let result;
 
-    if ((!num1 && !num2) && !(["Sin", "Cos", "Tan", "Square Root", "Round"].includes(operator))) {
-        result = "Plase enter 1st and 2nd number";
+    if (!inputValue) {
+        result = "Please enter the math problem";
         answer.value = result;
     } else {
-        switch (operator) {
-            case 'None':
-                result = 'Please select an operator';
-                break;
-            case 'Add':
-                result = num1 + num2;
-                break;
-            case 'Subtract':
-                result = num1 - num2;
-                break;
-            case 'Multiply':
-                result = num1 * num2;
-                break;
-            case 'Divide':
-                result = num1 / num2;
-                break;
-            case 'Sin':
-                result = Math.sin(num1);
-                break;
-            case 'Cos':
-                result = Math.cos(num1);
-                break;
-            case 'Tan':
-                result = Math.tan(num1);
-                break;
-            case 'Square Root':
-                result = Math.sqrt(num1);
-                break;
-            case 'Round':
-                result = Math.round(num1);
-                break;
-            default:
-                result = 'Please select an operator';
-                break;
+        try {
+            result = Function('"use strict"; return (' + inputValue + ')')();
+            answer.value = result;
+        } catch (error) {
+            result = "Invalid math expression";
+            answer.value = result;
         }
-    
-        answer.value = result;
-    }
-});
-
-document.querySelector('select[name="operator"]').addEventListener('change', (e) => {
-    e.preventDefault();
-    const num2 = form.querySelector('input[name="num2"]');
-    if (["Sin", "Cos", "Tan", "Square Root", "Round"].includes(e.target.value)) {
-        num2.hidden = true;
-    } else {
-        num2.hidden = false;
     }
 });
